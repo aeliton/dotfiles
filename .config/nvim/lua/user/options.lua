31ce1c97c20d11bff21e83e864182f82384ea4e3
Aeliton G. Silva <aeliton@gmail.com>
2025-05-02 16:47:22 -0300

    Fix options

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -4,6 +4,7 @@ vim.g.maplocalleader = '\\'
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
-vim.o.colorcolumn = "80"
+vim.o.colorcolumn = '80'
+vim.o.textwidth = 80
 
-vim.o.completeopt = "menu,popup,fuzzy,noselect"
+vim.o.completeopt = 'menu,popup,fuzzy,noselect'
