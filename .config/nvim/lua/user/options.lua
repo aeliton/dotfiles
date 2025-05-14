473b6b8e3bfdf427cb49958c4c3b0d7efe946e01
Aeliton G. Silva <aeliton@gmail.com>
2025-05-14 20:05:34 -0300

    Update options

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,10 +1,16 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
+vim.o.number = true
+
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
-vim.o.colorcolumn = '80'
 vim.o.textwidth = 80
+
+vim.o.colorcolumn = '80'
+
+vim.o.hlsearch = false
+vim.o.incsearch = true
 
 vim.o.completeopt = 'menu,popup,fuzzy,noselect'
