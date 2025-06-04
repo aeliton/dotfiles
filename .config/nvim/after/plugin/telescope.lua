6c2652a8f198bd6f221a0055bb82b939ddb58047
Aeliton G. Silva <aeliton@gmail.com>
2025-06-04 09:10:15 -0300

    Add mappings for telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,2 +1,4 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.grep_string, { desc = 'Ripgrep files' })
+vim.keymap.set('n', '<leader>be', builtin.buffers, { desc = 'Browse buffers' })
