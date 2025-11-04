c406052ec715ea9fc5ccd6ecac376f35be07834f
Aeliton G. Silva <aeliton@aeliton.com>
2025-11-04 18:15:31 -0300

    Update devenv function

--- .sh/build.sh
+++ .sh/build.sh
@@ -1,13 +1,13 @@
 function devenv() {
   if [ ! -e ${PWD}/devenv/bin/activate ]
   then
-    python3 -m venv devenv
-    source devenv/bin/activate
+    python3 -m venv .venv
+    source .venv/bin/activate
     python -m pip install pip --upgrade
     pip install wheel setuptools
     [[ -e ${PWD}/requirements.txt ]] && pip install -r requirements.txt
   else
-    source devenv/bin/activate
+    source .venv/bin/activate
   fi
 }
 
