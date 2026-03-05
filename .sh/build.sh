ab8a17a2cd8ef859a04f04c4149825099c8dee07
Aeliton G. Silva <aeliton@aeliton.com>
2026-03-04 22:12:04 -0300

    Fix neovim set spell

--- .sh/build.sh
+++ .sh/build.sh
@@ -1,13 +1,14 @@
+VENV_DIR=.venv
 function devenv() {
-  if [ ! -e ${PWD}/devenv/bin/activate ]
+  if [ ! -e ${PWD}/${VENV_DIR}/bin/activate ]
   then
-    python3 -m venv .venv
-    source .venv/bin/activate
+    python3 -m venv ${VENV_DIR}
+    source ${VENV_DIR}/bin/activate
     python -m pip install pip --upgrade
     pip install wheel setuptools
     [[ -e ${PWD}/requirements.txt ]] && pip install -r requirements.txt
   else
-    source .venv/bin/activate
+    source ${VENV_DIR}/bin/activate
   fi
 }
 
