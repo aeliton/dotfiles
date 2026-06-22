a09bdd07afa071ec71e97494ca4e8a2dedf2040b
Aeliton G. Silva <aeliton@aeliton.com>
2026-06-21 21:43:14 -0300

    Update changes

--- .config/sbuild/config.pl
+++ .config/sbuild/config.pl
@@ -1,8 +1,11 @@
 $chroot_mode = 'unshare';
 $external_commands = { "build-failed-commands" => [ [ '%SBUILD_SHELL' ] ] };
+$distribution = 'unstable';
+$build_dep_resolver = 'aptitude';
 $build_arch_all = 1;
 $build_source = 1;
 $source_only_changes = 1;
 $run_lintian = 1;
 $lintian_opts = ['--display-info', '--verbose', '--fail-on', 'error,warning', '--info'];
 $run_autopkgtest = 1;
+$autopkgtest_opts = ['--apt-upgrade', '--', 'unshare', '--release', '%r', '--arch', '%a' ];
